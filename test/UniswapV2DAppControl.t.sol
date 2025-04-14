a5b044a5ffd2e2696fd901ebd00e95fc9fbd0a94
marshabl <marshabl12@gmail.com>
2025-04-14 13:03:55 -0700

    fix monaad testing

--- test/UniswapV2DAppControl.t.sol
+++ test/UniswapV2DAppControl.t.sol
@@ -6,6 +6,8 @@ import { console } from "forge-std/console.sol";
 import { BaseTest } from "@atlas-test/base/BaseTest.t.sol";
 
 import { SolverBase } from "@atlas/solver/SolverBase.sol";
+import { IAtlas } from "@atlas/interfaces/IAtlas.sol";
+import { IAtlasVerification } from "@atlas/interfaces/IAtlasVerification.sol";
 import { TxBuilder } from "@atlas/helpers/TxBuilder.sol";
 
 import { SolverOperation } from "@atlas/types/SolverOperation.sol";
@@ -16,14 +18,18 @@ import { IUniswapV2Router02 } from "../src/interfaces/IUniswapV2Router.sol";
 import { IUniswapV2Pair } from "../src/interfaces/IUniswapV2Pair.sol";
 import { IUniswapV2Factory } from "../src/interfaces/IUniswapV2Factory.sol";
 import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
 import { SwapMath } from "../src/SwapMath.sol";
 
 // Uniswap V2 mainnet addresses
 address constant SWAP_ROUTER = 0xCa810D095e90Daae6e867c19DF6D9A8C56db2c89; // Uniswap V2 Router
 address constant FACTORY = 0x0085388Da29e74b66ac6b6fF690973bE05403f67;
-address constant ATLAS = 0x81E8c782Abbe606366C246C6c85475009ff6fb84;
+address constant ATLAS_ADDRESS = 0x81E8c782Abbe606366C246C6c85475009ff6fb84;
+address constant ATLAS_VERIFICATION_ADDRESS = 0xb47387995e866908B25b49e8BaC7e499170461A6;
 
 IUniswapV2Router02 constant ROUTER = IUniswapV2Router02(SWAP_ROUTER);
+IAtlas constant ATLAS = IAtlas(ATLAS_ADDRESS);
+IAtlasVerification constant ATLAS_VERIFICATION = IAtlasVerification(ATLAS_VERIFICATION_ADDRESS);
 
 // Token addresses
 address constant WETH_ADDRESS = 0x760AfE86e5de5fa0Ee542fc7B7B713e1c5425701; // Mainnet WETH
@@ -57,9 +63,7 @@ contract UniswapV2DAppControlTest is BaseTest {
     Sig sig;
 
     function setUp() public virtual override {
-        // super.setUp(); // Use the BaseTest setUp
         __createAndLabelAccounts();
-        __deployAtlasContracts();
         
         // Create governance
         governancePK = 11_112;
@@ -67,20 +71,20 @@ contract UniswapV2DAppControlTest is BaseTest {
 
         // Deploy the control contract
         vm.startPrank(governanceEOA);
-        control = new UniswapV2DAppControl(address(atlas), ETH, governanceEOA, 5000, 0.005 ether); //50% gov payout
-        atlasVerification.initializeGovernance(address(control));
+        control = new UniswapV2DAppControl(address(ATLAS), ETH, governanceEOA, 5000, 0.005 ether); //50% gov payout
+        ATLAS_VERIFICATION.initializeGovernance(address(control));
         vm.stopPrank();
 
         // Create execution environment
         vm.startPrank(userEOA);
-        executionEnvironment = atlas.createExecutionEnvironment(userEOA, address(control));
+        executionEnvironment = ATLAS.createExecutionEnvironment(userEOA, address(control));
         vm.stopPrank();
 
         // Setup txBuilder helper
         txBuilder = new TxBuilder({
             _control: address(control),
-            _atlas: address(atlas),
-            _verification: address(atlasVerification)
+            _atlas: address(ATLAS),
+            _verification: address(ATLAS_VERIFICATION)
         });
 
         // Get contract parameters
@@ -104,8 +108,8 @@ contract UniswapV2DAppControlTest is BaseTest {
             // Add funding for governance
         deal(governanceEOA, 2e18);
         vm.startPrank(governanceEOA);
-        atlas.deposit{ value: 1e18 }();
-        atlas.bond(1e18);
+        ATLAS.deposit{ value: 1e18 }();
+        ATLAS.bond(1e18);
         vm.stopPrank();
     }
 
@@ -129,10 +133,10 @@ contract UniswapV2DAppControlTest is BaseTest {
         
         // Do the actual metacall
         vm.startPrank(userEOA);
-        IERC20(tokenIn).approve(address(atlas), amountIn);
+        IERC20(tokenIn).approve(address(ATLAS), amountIn);
         
         // Use try/catch to get error details
-        atlas.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
+        ATLAS.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
         
         vm.stopPrank();
 
@@ -162,8 +166,8 @@ contract UniswapV2DAppControlTest is BaseTest {
 
         // Do the actual metacall
         vm.startPrank(userEOA);
-        IERC20(tokenIn).approve(address(atlas), amountInMax);
-        atlas.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
+        IERC20(tokenIn).approve(address(ATLAS), amountInMax);
+        ATLAS.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
         vm.stopPrank();
 
         uint256 userTokenBalanceAfter = _balanceOf(tokenOut, userEOA);
@@ -191,7 +195,7 @@ contract UniswapV2DAppControlTest is BaseTest {
 
         // Do the actual metacall
         vm.startPrank(userEOA);
-        atlas.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
+        ATLAS.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
         vm.stopPrank();
 
         uint256 userTokenBalanceAfter = _balanceOf(tokenOut, userEOA);
@@ -228,8 +232,8 @@ contract UniswapV2DAppControlTest is BaseTest {
 
         // Do the actual metacall
         vm.startPrank(userEOA);
-        IERC20(DAI_ADDRESS).approve(address(atlas), amountIn);
-        atlas.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
+        IERC20(DAI_ADDRESS).approve(address(ATLAS), amountIn);
+        ATLAS.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
         vm.stopPrank();
 
         uint256 userEthBalanceAfter = _balanceOf(ETH, userEOA);
@@ -262,8 +266,8 @@ contract UniswapV2DAppControlTest is BaseTest {
 
         // Do the actual metacall
         vm.startPrank(userEOA);
-        IERC20(DAI_ADDRESS).approve(address(atlas), amountOut);
-        atlas.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
+        IERC20(DAI_ADDRESS).approve(address(ATLAS), amountOut);
+        ATLAS.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
         vm.stopPrank();
 
         uint256 userEthBalanceAfter = _balanceOf(ETH, userEOA);
@@ -295,7 +299,7 @@ contract UniswapV2DAppControlTest is BaseTest {
 
         // Do the actual metacall
         vm.startPrank(userEOA);
-        atlas.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
+        ATLAS.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
         vm.stopPrank();
 
         uint256 userTokenBalanceAfter = _balanceOf(tokenOut, userEOA);
@@ -324,8 +328,8 @@ contract UniswapV2DAppControlTest is BaseTest {
 
         // Do the actual metacall
         vm.startPrank(userEOA);
-        IERC20(tokenIn).approve(address(atlas), amountIn);
-        atlas.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
+        IERC20(tokenIn).approve(address(ATLAS), amountIn);
+        ATLAS.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
         vm.stopPrank();
 
         uint256 userTokenBalanceAfter = _balanceOf(tokenOut, userEOA);
@@ -353,7 +357,7 @@ contract UniswapV2DAppControlTest is BaseTest {
 
         // Do the actual metacall
         vm.startPrank(userEOA);
-        atlas.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
+        ATLAS.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
         vm.stopPrank();
 
         uint256 userTokenBalanceAfter = _balanceOf(tokenOut, userEOA);
@@ -386,8 +390,8 @@ contract UniswapV2DAppControlTest is BaseTest {
 
         // Do the actual metacall
         vm.startPrank(userEOA);
-        IERC20(DAI_ADDRESS).approve(address(atlas), amountOut);
-        atlas.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
+        IERC20(DAI_ADDRESS).approve(address(ATLAS), amountOut);
+        ATLAS.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
         vm.stopPrank();
 
         uint256 userEthBalanceAfter = _balanceOf(ETH, userEOA);
@@ -416,8 +420,8 @@ contract UniswapV2DAppControlTest is BaseTest {
 
         // Do the actual metacall
         vm.startPrank(userEOA);
-        IERC20(tokenIn).approve(address(atlas), amountIn);
-        atlas.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
+        IERC20(tokenIn).approve(address(ATLAS), amountIn);
+        ATLAS.metacall{ value: msgValue }(userOp, solverOps, dAppOp, address(0));
         vm.stopPrank();
 
         uint256 userTokenBalanceAfter = _balanceOf(DAI_ADDRESS, userEOA);
@@ -468,7 +472,7 @@ contract UniswapV2DAppControlTest is BaseTest {
         dAppOp = txBuilder.buildDAppOperation(governanceEOA, userOp, solverOps);
         dAppOp.bundler = userEOA;
 
-        (sig.v, sig.r, sig.s) = vm.sign(governancePK, atlasVerification.getDAppOperationPayload(dAppOp));
+        (sig.v, sig.r, sig.s) = vm.sign(governancePK, ATLAS_VERIFICATION.getDAppOperationPayload(dAppOp));
         dAppOp.signature = abi.encodePacked(sig.r, sig.s, sig.v);
         return (userOp, solverOps, dAppOp);
     }
@@ -485,18 +489,18 @@ contract UniswapV2DAppControlTest is BaseTest {
     {
         vm.startPrank(solverEOA);
         // Make sure solver has 1 AtlETH bonded in Atlas
-        uint256 bonded = atlas.balanceOfBonded(solverEOA);
+        uint256 bonded = ATLAS.balanceOfBonded(solverEOA);
         if (bonded < 1e18) {
-            uint256 atlETHBalance = atlas.balanceOf(solverEOA);
+            uint256 atlETHBalance = ATLAS.balanceOf(solverEOA);
             if (atlETHBalance < 1e18) {
                 deal(solverEOA, 1e18 - atlETHBalance);
-                atlas.deposit{ value: 1e18 - atlETHBalance }();
+                ATLAS.deposit{ value: 1e18 - atlETHBalance }();
             }
-            atlas.bond(1e18 - bonded);
+            ATLAS.bond(1e18 - bonded);
         }
 
         // Deploy solver contract
-        MockETHSolver solver = new MockETHSolver(ETH, address(atlas));
+        MockETHSolver solver = new MockETHSolver(ETH, address(ATLAS));
         solver.setShouldSucceed(shouldSucceed);
 
         // Give bidAmount of ETH to solver contract
@@ -536,7 +540,7 @@ contract UniswapV2DAppControlTest is BaseTest {
         });
 
         // Sign solverOp
-        (sig.v, sig.r, sig.s) = vm.sign(solverPK, atlasVerification.getSolverPayload(solverOp));
+        (sig.v, sig.r, sig.s) = vm.sign(solverPK, ATLAS_VERIFICATION.getSolverPayload(solverOp));
         solverOp.signature = abi.encodePacked(sig.r, sig.s, sig.v);
     }
 }
